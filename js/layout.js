b2a53f307d391b3ea8fe92a9f8601c31d1f68e87
Yaron Weiss <YARON.WEISS@amdocs.com>
2018-06-20 11:55:15 -0500

    changed layout and docs html

--- js/layout.js
+++ js/layout.js
@@ -3,8 +3,9 @@ function setLayout()
 				$("#headerdiv").append("<div style='border: 1px solid; background:lightgray; height: 150px; width: 15%; float: left;'><img src='./images/GingerIcon.png' width='170' height='150'/></div>");	
 
 				h = "<div style='border: 1px solid; height: 150px; width: 60%; float: left; background:orange;'>";
-				h += "<h1>Ginger Automation</h1>"
-				// h += "<h4>Spice it up!</h4>"
+				// h += "<h1>Ginger Automation</h1>"
+				
+				h += "<h4>Spice it up!</h4>"
 
 				s= '<nav><ul>';
 				s+='<li><a href="./index.html">Home</li>';
@@ -21,6 +22,7 @@ function setLayout()
 				
 				h2 = "<div style='border: 1px solid; height: 150px; width: 20%; float: right; background:lightgray;'>";
 				h2+= "<img src='./images/amdocs.png' width='100' height='100'/>"
+				h2+= "</br>";
 				h2+= "<label>by amdocs</label>";
 				h2 += "</div>"
 				
