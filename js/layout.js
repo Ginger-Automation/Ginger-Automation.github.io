66071dff8a41732645a204c4089d975cc939b9bb
Ornit Weiss <oweiss@illinois.edu>
2018-06-21 13:13:16 -0500

    first try

--- js/layout.js
+++ js/layout.js
@@ -1,10 +1,14 @@
 function setLayout()
 			{
-				$("#headerdiv").append("<div style='border: 1px solid; background:lightgray; height: 150px; width: 15%; float: left;'><img src='./images/GingerIcon.png' width='170' height='150'/></div>");	
+    $("#headerdiv").append(
+        //"<div style='border: 1px solid; background:lightgray; height: 150px; width: 15%; float: left;'><img src='./images/GingerIcon.png' width='170' height='150'/></div>"
+        "<div id ='logo'><img src='./images/GingerIcon.png' width='170' height='150' title='GingerIcon' alt='Ginger Icon' /></div>"
+    );	
 
-				h = "<div style='border: 1px solid; height: 150px; width: 60%; float: left; background:orange;'>";
+				//h = "<div style='border: 1px solid; height: 150px; width: 60%; float: left; background:orange;'>";
 				// h += "<h1>Ginger Automation</h1>"
-				
+                h = "<div id='headercenter'>";
+
 				h += "<h4>Spice it up!</h4>"
 
 				s= '<nav><ul>';
@@ -20,8 +24,9 @@ function setLayout()
 				h += "</div>"
 				$("#headerdiv").append(h);	
 				
-				h2 = "<div style='border: 1px solid; height: 150px; width: 20%; float: right; background:lightgray;'>";
-				h2+= "<img src='./images/amdocs.png' width='100' height='100'/>"
+                //h2 = "<div style='border: 1px solid; height: 150px; width: 20%; float: right; background:lightgray;'>";
+                h2 = "<div id ='amdocsIcon'>";
+                h2 += "<img src='./images/amdocsIcon.png' width='175' height='100' title='amdocsIcon' alt='amdocs Icon'/>"
 				h2+= "</br>";
 				h2+= "<label>by amdocs</label>";
 				h2 += "</div>"
